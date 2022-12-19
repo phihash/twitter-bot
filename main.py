ed0ae91c62f917a9a8d7ab41f1ac4908317407b5
phihash <insharp0220@gmail.com>
2022-12-20 02:05:21 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from config import CONFIG
+import tweepy ,os
+
+CK = os.environ.get("TWITTER_API_CONSUMER_KEY")
+CS = os.environ.get("TWITTER_API_CONSUMER_SECRET")
+AT = os.environ.get("TWITTER_API_ACCESS_TOKEN_KEY")
+AS = os.environ.get("TWITTER_API_ACCESS_TOKEN_SECRET")
+
+
+auth = tweepy.OAuthHandler(CK,CS)
+
+auth.set_access_token(AT,AS)
+
+api = tweepy.API(auth)
+
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print('-------------------------')
+#     print(tweet.text)
+
+for tweet in tweepy.Cursor(api.search_tweets, q='幸せ').items(10):
+    print(tweet.user.name+"さんが",tweet.text,"と呟きました")
+
+
+# TWEET_TEXT = "Hello World!"
+
+# api.update_status(TWEET_TEXT)
